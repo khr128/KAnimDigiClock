1a70e6f1f0bf32f1c9ed5ad2b95ae1fa9195674d
Dmitri Khrebtukov <khr128@gmail.com>
2016-04-29 10:33:55 -0700

    Initial Commit

---
+++ KAnimDigiClock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KAnimDigiClock
+//
+//  Created by khr on 4/29/16.
+//  Copyright © 2016 khr. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
